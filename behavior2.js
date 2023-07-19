b298c020c567dade71d0fe8d8bf032029a4a3fef
Chrissy Gonzalez <chrissy.gonzalez@electric.ai>
2023-07-19 16:39:46 -0400

    Start working on color theme buttons

--- behavior2.js
+++ behavior2.js
@@ -1,28 +1,63 @@
+const [body] = document.getElementsByTagName("body");
 const [h1] = document.getElementsByTagName("h1");
 const [h2] = document.getElementsByTagName("h2");
 const [main] = document.getElementsByTagName("main");
 const [content] = document.getElementsByClassName("content");
 
-const [pinkButton, blackButton, blueButton, invertButton, editButton] =
-  document.getElementsByTagName("button");
+const [
+  pinkButton,
+  blackButton,
+  blueButton,
+  invertButton,
+  serifButton,
+  chiseledButton,
+  wideButton,
+  editButton,
+] = document.getElementsByTagName("button");
+
+const [theme1, theme2, theme3] = document.getElementsByClassName("color-theme");
+const [left1, left2, left3] =
+  document.getElementsByClassName("color-theme-left");
+const [right1, right2, right3] =
+  document.getElementsByClassName("color-theme-right");
+
+const pink = "#ffcfd5";
+const red = "#de1447";
+const black = "#000";
+const white = "#fff";
+const blue = "#4b49b4";
+const lime = "#ccff00";
 
 const editMenu = document.getElementById("edit-menu");
 let shouldShow = true;
 
+pinkButton.style.setProperty("background-color", pink);
+blackButton.style.setProperty("background-color", black);
+blueButton.style.setProperty("background-color", blue);
+
 const toggleEditing = () => {
   if (shouldShow) {
-    content.classList.add("squish");
+    content.classList.add("content-show-edit");
     editMenu.classList.add("show-edit");
     editButton.classList.add("editing");
     editButton.innerHTML = "&larr;";
     shouldShow = !shouldShow;
   } else {
-    content.classList.remove("squish");
+    content.classList.remove("content-show-edit");
     editMenu.classList.remove("show-edit");
     editButton.classList.remove("editing");
     editButton.innerHTML = "&rarr;";
     shouldShow = !shouldShow;
   }
+};
+
+const changeColor = (textColor, bgColor) => {
+  editButton.style.setProperty("--main-text-color", textColor);
+  editButton.style.setProperty("--main-bg-color", bgColor);
+  h1.style.setProperty("--main-text-color", textColor);
+  h2.style.setProperty("--main-text-color", textColor);
+  main.style.setProperty("--main-bg-color", bgColor);
+  body.style.setProperty("--main-bg-color", bgColor);
 };
 
 const invertColors = () => {
@@ -33,19 +68,22 @@ const invertColors = () => {
   changeColor(bgColor, textColor);
 };
 
-pinkButton.addEventListener("click", () => changeColor("#c70f18", "#ffb0b1"));
-blackButton.addEventListener("click", () => changeColor("#fff", "#000"));
-blueButton.addEventListener("click", () => changeColor("#ccff00", "#4b49b4"));
-invertButton.addEventListener("click", invertColors);
+const changeFont = (font) => {
+  h1.style.setProperty("--font-family", font);
+  h2.style.setProperty("--font-family", font);
+};
 
 editButton.addEventListener("click", () => {
   toggleEditing();
 });
 
-const changeColor = (textColor, bgColor) => {
-  editButton.style.setProperty("--main-text-color", textColor);
-  editButton.style.setProperty("--main-bg-color", bgColor);
-  h1.style.setProperty("--main-text-color", textColor);
-  h2.style.setProperty("--main-text-color", textColor);
-  main.style.setProperty("--main-bg-color", bgColor);
-};
+pinkButton.addEventListener("click", () => changeColor(red, pink));
+blackButton.addEventListener("click", () => changeColor(white, black));
+blueButton.addEventListener("click", () => changeColor(lime, blue));
+invertButton.addEventListener("click", invertColors);
+
+serifButton.addEventListener("click", () => changeFont("Caprasimo, cursive"));
+chiseledButton.addEventListener("click", () =>
+  changeFont("Bellota, sans-serif")
+);
+wideButton.addEventListener("click", () => changeFont("Unbounded, cursive"));
