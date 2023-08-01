9b4cc3ba8c2a1626ee02adf511c9d872af4a8089
Chrissy Gonzalez <chrissy.gonzalez@electric.ai>
2023-08-01 13:36:27 -0400

    Get color pickers working

--- behavior2.js
+++ behavior2.js
@@ -1,89 +1,128 @@
+// color constants
+const pink = "#ffcfd5";
+const red = "#de1447";
+const black = "#000000";
+const white = "#ffffff";
+const blue = "#4b49b4";
+const lime = "#ccff00";
+const foregrounds = [red, white, lime];
+const backgrounds = [pink, black, blue];
+
+// get all the elements
 const [body] = document.getElementsByTagName("body");
 const [h1] = document.getElementsByTagName("h1");
 const [h2] = document.getElementsByTagName("h2");
 const [main] = document.getElementsByTagName("main");
 const [content] = document.getElementsByClassName("content");
-
-const [
-  pinkButton,
-  blackButton,
-  blueButton,
-  invertButton,
-  serifButton,
-  chiseledButton,
-  wideButton,
-  editButton,
-] = document.getElementsByTagName("button");
-
-const [theme1, theme2, theme3] = document.getElementsByClassName("color-theme");
-const [left1, left2, left3] =
-  document.getElementsByClassName("color-theme-left");
-const [right1, right2, right3] =
-  document.getElementsByClassName("color-theme-right");
-
-const pink = "#ffcfd5";
-const red = "#de1447";
-const black = "#000";
-const white = "#fff";
-const blue = "#4b49b4";
-const lime = "#ccff00";
-
 const editMenu = document.getElementById("edit-menu");
-let shouldShow = true;
+const foregroundPicker = document.getElementById("foreground");
+const backgroundPicker = document.getElementById("background");
+const [invertButton, serifButton, chiseledButton, wideButton, editButton] =
+  document.getElementsByTagName("button");
+const [theme1, theme2, theme3] = document.getElementsByClassName("color-theme");
+const foregroundItems = [h1, h2, editButton];
+const backgroundItems = [body, main, editButton];
 
-pinkButton.style.setProperty("background-color", pink);
-blackButton.style.setProperty("background-color", black);
-blueButton.style.setProperty("background-color", blue);
+// draw the little theme things
+[...document.getElementsByClassName("color-theme-left")].forEach(
+  (elem, index) =>
+    elem.style.setProperty("background-color", backgrounds[index])
+);
+[...document.getElementsByClassName("color-theme-right")].forEach(
+  (elem, index) =>
+    elem.style.setProperty("background-color", foregrounds[index])
+);
 
+// color, font, and edit state
+const pageState = {
+  showEdit: false,
+  foregroundColor: lime,
+  backgroundColor: blue,
+};
+
+// functions to control edit mode
 const toggleEditing = () => {
-  if (shouldShow) {
+  pageState.showEdit = !pageState.showEdit;
+  displayCurrentEditState();
+};
+
+const displayCurrentEditState = () => {
+  if (pageState.showEdit) {
     content.classList.add("content-show-edit");
     editMenu.classList.add("show-edit");
     editButton.classList.add("editing");
     editButton.innerHTML = "&larr;";
-    shouldShow = !shouldShow;
   } else {
     content.classList.remove("content-show-edit");
     editMenu.classList.remove("show-edit");
     editButton.classList.remove("editing");
     editButton.innerHTML = "&rarr;";
-    shouldShow = !shouldShow;
   }
 };
 
-const changeColor = (textColor, bgColor) => {
-  editButton.style.setProperty("--main-text-color", textColor);
-  editButton.style.setProperty("--main-bg-color", bgColor);
-  h1.style.setProperty("--main-text-color", textColor);
-  h2.style.setProperty("--main-text-color", textColor);
-  main.style.setProperty("--main-bg-color", bgColor);
-  body.style.setProperty("--main-bg-color", bgColor);
+// functions to do things with color
+const setColors = ({
+  foreground = pageState.foregroundColor,
+  background = pageState.backgroundColor,
+}) => {
+  pageState.foregroundColor = foreground;
+  pageState.backgroundColor = background;
+  changeColors();
+};
+
+const changeColors = () => {
+  foregroundItems.forEach((item) =>
+    item.style.setProperty("--main-text-color", pageState.foregroundColor)
+  );
+  backgroundItems.forEach((item) =>
+    item.style.setProperty("--main-bg-color", pageState.backgroundColor)
+  );
+  foregroundPicker.value = pageState.foregroundColor;
+  backgroundPicker.value = pageState.backgroundColor;
 };
 
 const invertColors = () => {
-  const bgColor =
+  const foreground =
     getComputedStyle(editButton).getPropertyValue("--main-bg-color");
-  const textColor =
+  const background =
     getComputedStyle(editButton).getPropertyValue("--main-text-color");
-  changeColor(bgColor, textColor);
+  setColors({ foreground, background });
 };
 
+function pickForeground() {
+  setColors({ foreground: this.value });
+}
+
+function pickBackground() {
+  setColors({ background: this.value });
+}
+
+// functions to do things with fonts
 const changeFont = (font) => {
   h1.style.setProperty("--font-family", font);
   h2.style.setProperty("--font-family", font);
 };
 
+// register event listeners
 editButton.addEventListener("click", () => {
   toggleEditing();
 });
-
-pinkButton.addEventListener("click", () => changeColor(red, pink));
-blackButton.addEventListener("click", () => changeColor(white, black));
-blueButton.addEventListener("click", () => changeColor(lime, blue));
+theme1.addEventListener("click", () =>
+  setColors({ foreground: red, background: pink })
+);
+theme2.addEventListener("click", () =>
+  setColors({ foreground: white, background: black })
+);
+theme3.addEventListener("click", () =>
+  setColors({ foreground: lime, background: blue })
+);
 invertButton.addEventListener("click", invertColors);
-
 serifButton.addEventListener("click", () => changeFont("Caprasimo, cursive"));
 chiseledButton.addEventListener("click", () =>
   changeFont("Bellota, sans-serif")
 );
 wideButton.addEventListener("click", () => changeFont("Unbounded, cursive"));
+foregroundPicker.addEventListener("input", pickForeground);
+backgroundPicker.addEventListener("input", pickBackground);
+
+changeColors();
